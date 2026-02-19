e4df15584d680d422f4ff8263c81e09d2e2408c5
buvidk <1532493290@qq.com>
2026-02-19 10:20:19 +0800

    chore(test): move vector store tests to appropiate position

--- vector-stores/spring-ai-alibaba-tablestore-store/src/test/java/com/alibaba/cloud/ai/vectorstore/tablestore/example/KnowledgeStoreInitExample.java
+++ vector-stores/spring-ai-alibaba-tablestore-store/src/test/java/com/alibaba/cloud/ai/vectorstore/tablestore/example/KnowledgeStoreInitExample.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.cloud.ai.vectorstore.tablestore.autoconfigure.example;
+package com.alibaba.cloud.ai.vectorstore.tablestore.example;
 
 import com.alicloud.openservices.tablestore.SyncClient;
 import com.alicloud.openservices.tablestore.model.search.FieldSchema;
